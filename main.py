ace7cb89fcc86ab5c90924f188358fba7bc3fdc5
Grégory Ployart <gregory.ployartwork@gmail.com>
2022-07-18 12:21:04 +0200

    final commit for v1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-import pyforms
+from pyforms import start_app
 from win32api import GetSystemMetrics
 
 from src.app.UI.MainWindow import MainWindow
 
 if __name__ == '__main__':
-    pyforms.start_app(MainWindow,
-                      geometry=(int(GetSystemMetrics(0) / 2) - 160,
-                                int(GetSystemMetrics(1) / 2) - 200,
-                                320,
-                                400))
+    start_app(MainWindow,
+              geometry=(int(GetSystemMetrics(0) / 2) - 160,
+                        int(GetSystemMetrics(1) / 2) - 200,
+                        320,
+                        400))
